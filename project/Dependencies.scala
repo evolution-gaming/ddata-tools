e7e105b26275cc65339dcbcc2142a73ac7ebc935
Yaroslav Klymko <t3hnar@gmail.com>
2019-07-17 13:19:46 +0200

    bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object Dependencies {
 
   val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.1"
-  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.7"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "0.0.12"
+  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.8"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "0.0.18"
 
   object Cats {
-    private val version = "1.6.0"
-    val core   = "org.typelevel" %% "cats-core" % version
+    private val version = "1.6.1"
+    val core   = "org.typelevel" %% "cats-core"   % version
     val effect = "org.typelevel" %% "cats-effect" % "1.3.1"
   }
 
