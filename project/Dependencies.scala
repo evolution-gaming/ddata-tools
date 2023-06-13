28b2ce10de3f97ad98147cce193025a0d4ed586e
Ilya Surkov <ilyas9406@yandex.ru>
2023-06-13 17:46:57 +0200

    Update dependencies, migrate to cats-helper MeasureDuration (#3)
    * Update dependencies, migrate to cats-helper MeasureDuration
    
    * Fix scala versions in CI
    
    * Bump plugin versions
    
    (cherry picked from commit fb5255b0a81b2497ea6f75279a9d6a705fbf8e44)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,17 +4,16 @@ object Dependencies {
 
   val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.2"
   val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.8"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "2.2.3"
-  val smetrics         = "com.evolutiongaming" %% "smetrics"       % "0.3.2"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "2.11.0"
+  val smetrics         = "com.evolutiongaming" %% "smetrics"       % "0.4.2"
 
   object Cats {
-    private val version = "2.3.0"
-    val core   = "org.typelevel" %% "cats-core"   % version
-    val effect = "org.typelevel" %% "cats-effect" % version
+    val core   = "org.typelevel" %% "cats-core"   % "2.9.0"
+    val effect = "org.typelevel" %% "cats-effect" % "2.5.5"
   }
 
   object Akka {
-    private val version = "2.6.8"
+    private val version = "2.6.20"
     val actor              = "com.typesafe.akka" %% "akka-actor"            % version
     val cluster            = "com.typesafe.akka" %% "akka-cluster"          % version
     val `distributed-data` = "com.typesafe.akka" %% "akka-distributed-data" % version
